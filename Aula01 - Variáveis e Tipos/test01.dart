1d31a1187a7337459240741e121c4c2597267774
lucaspbds <118389176+lucaspbds@users.noreply.github.com>
2024-03-28 11:48:20 -0300

    First commit

---
+++ Aula01 - Variáveis e Tipos/test01.dart
@@ -0,0 +1,9 @@
+void main(){
+  String name = "David Lucas";
+  int idade = 17;
+  bool isTrue = true;
+  List <int> numeros = [5, 6, 8];
+  List listaDePalavras = ["Banana", "Pedra", "Terra"];
+  print(listaDePalavras);
+  print(numeros);
+  }
